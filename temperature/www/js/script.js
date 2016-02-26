bc44969eeb7a304594a4efc57905a0b066ae2e65
Oliver Füllemann <oliver_10@hotmail.ch>
2016-02-26 15:04:03 +0100

    Scripterion changerinos MLG

--- temperature/www/js/script.js
+++ temperature/www/js/script.js
@@ -3,9 +3,7 @@
     $.ajax({
       type:"GET",
       url:"../www/delete.php",
-      data:{id:id},
-      success:function(data){
-        console.log(data);
+      success:function(){
         refreshTable();
       },
       error:function(data){
@@ -18,31 +16,50 @@
   function insertEintrag(){
     var tmp = $('#temperature').val();
     console.log(tmp);
-    var check = $('#temperature').val();
-    if(parseInt(check.value) < 61 || check.value > -51){
-      $.ajax({
-        type:"POST",
-        url:"insert.php",
-        data: {tmp:tmp},
-        success:function(data){
-          console.log(data);
-          $("#temperature").val(null);
-        },
-        error:function(error){
-          console.log(error);
-          $("#temperature").val(null);
-        }
+    $.ajax({
+      type:"POST",
+      url:"insert.php",
+      data: {tmp:tmp},
+      success:function(data){
+        console.log(data);
+        $("#temperature").val(null);
+      },
+      error:function(error){
+        console.log(error);
+      }
+    })
+  };
+
+$(document).ready(function refreshTable(resultarray2){
+  $.ajax({
+    type:"GET",
+    url:"../www/read_json.php",
+    success:function(data){
+      console.log(data);
+      var tabelle = "";
+      $.each(JSON.parse(data), function(id, obj){
+        tabelle += "<tr>\
+        <td>"+obj.ID+"</td>\
+        <td>"+obj.Temperatur+"</td>\
+        <td>"+obj.Date+"</td>\
+        <td class='button'><button class='btn btn-danger' id='.$ID.' onclick='deleteEintrag('.$ID.')'>Delete</button></td>\
+        </tr>"
       });
-       }
-  };
 
-  function refreshTable(){
-
-  };
+      $("#ausgabe").html(tabelle);
+    },
+    error:function(data){
+      console.log(data);
+    },
+    beforeSend:function(){
+      $("#ausgabe").html = ("");
+    }
+  });
+});
 
   $('form').submit(function(e) {
           e.preventDefault();
           // do something else here// some errors are visible :)
           console.log('Prevent default ok');
           return false;
-  });
+  })
