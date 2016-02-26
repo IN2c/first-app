ee5d6d4c067209bab9fb1d302f54621a7ed3cf69
Valentin Baumann <sirzockholm@gmail.com>
2016-02-26 08:30:13 +0100

    added index.php to index.php
    Works! :smiley:

--- temperature/www/js/script.js
+++ temperature/www/js/script.js
@@ -7,10 +7,33 @@
       success:function(data){
         console.log(data);
         window.location.reload();
-        alert(data);
+        alert("WUHUUUUU");
       },
       error:function(){
         alert("something failed...");
       }
     });
   };
+
+  function insertEintrag(){
+    var tmp = $('#temperature').val();
+    console.log(tmp);
+    $.ajax({
+      type:"POST",
+      url:"insert.php",
+      data: {tmp:tmp},
+      success:function(data){
+        console.log(data);
+      },
+      error:function(error){
+        console.log(error);
+      }
+    })
+  };
+
+  $('form').submit(function(e) {
+          e.preventDefault();
+          // do something else here// some errors are visible :)
+          console.log('Prevent default ok');
+          return false;
+  })
