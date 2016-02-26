5ee524f4a240ad381c17082098d02b2ef6d61479
Valentin Baumann <sirzockholm@gmail.com>
2016-02-26 10:29:59 +0100

    Delete buttons

--- temperature/www/read.php
+++ temperature/www/read.php
@@ -3,12 +3,40 @@ require('connect.php');
  ?>
 <html>
 <head>
-  <script type="text/javascript" src="http://code.jquery.com/jquery-2.2.1.min.js"></script>
-  <script type="text/javascript" src="js/script.js"></script>
+  <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+
+  <meta name="description" content="">
+  <meta name="author" content="">
+  <link rel="icon" href="#/favicon.ico">
+  <title>Theme Template for Bootstrap</title>
+
+  <link href="css/bootstrap.min.css" rel="stylesheet">
+  <link href="css/bootstrap-theme.min.css" rel="stylesheet">
+  <link href="css/index.css" rel="stylesheet">
 </head>
+
 <body>
-  <div id='message'>
-  </div>
+  <nav class="navbar navbar-inverse navbar-fixed-top">
+    <div class="container">
+      <div class="navbar-header">
+        <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+          <span class="sr-only">Toggle navigation</span>
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+        </button>
+      </div>
+      <div id="navbar" class="navbar-collapse collapse">
+        <ul class="nav navbar-nav">
+          <li><a href="index.php">Eingabe</a></li>
+          <li class="active"><a href="read.php">&Uuml;bersicht</a></li>
+        </ul>
+      </div>
+    </div>
+  </nav>
   <?php
 
     $sql = "SELECT * FROM eintrag";
@@ -21,8 +49,11 @@ require('connect.php');
       echo "<td>".$temp." "."</td>";
       $datum = $row["Date"];
       echo "<td>".$datum."</td>";
-      echo "<td><button id=".$ID." onclick='deleteEintrag(".$ID.")'>Delete</button></td></tr></br>";
+      echo "<td><button class=\"btn btn-danger\" id=".$ID." onclick='deleteEintrag(".$ID.")'>Delete</button></td></tr></br>";
     }
   ?>
+  <script src="js/jquery.min.js"></script>
+  <script src="js/bootstrap.min.js"></script>
+  <script src="js/script.js"></script>
 </body>
 </html>
