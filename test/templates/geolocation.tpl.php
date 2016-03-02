e6e496c1bbf1762525ec2384349ec5860570b016
Toubi01 <Tobias@Tobiass-MBP.home>
2016-03-02 23:01:31 +0100

    überflüssige Ausgabe entfernt

--- test/templates/geolocation.tpl.php
+++ test/templates/geolocation.tpl.php
@@ -74,12 +74,8 @@
     }
 
     </script>
-
-<<<<<<< HEAD
-	   <script src="js/jquery.min.js"></script>
-=======
-	<script src="js/jquery.min.js"></script>
->>>>>>> origin/master
+    <script src="js/jquery.min.js"></script>
+    <script src="js/jquery.min.js"></script>
     <script src="js/bootstrap.min.js"></script>
     <script src="js/script.js"></script>
   </body>
